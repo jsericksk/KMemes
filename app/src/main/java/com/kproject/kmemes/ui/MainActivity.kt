b1c59b7bec4c164d444facf529658cafa9ad81fb
jsericksk <jsericksk@gmail.com>
2021-08-07 02:56:03 -0300

    Import fix

--- app/src/main/java/com/kproject/kmemes/ui/MainActivity.kt
+++ app/src/main/java/com/kproject/kmemes/ui/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.kproject.kmemes.R
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import com.kproject.kmemes.navigation.Navigation
-import com.kproject.kmemes.ui.screens.ImageListScreen
 import com.kproject.kmemes.ui.theme.KMemesTheme
 
 class MainActivity : ComponentActivity() {
